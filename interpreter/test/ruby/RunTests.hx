fc069b660cd74ae6b432b681aec17a2da442ce8b
Josh Cheek <josh.cheek@gmail.com>
2014-12-15 22:22:06 -0700

    Get an inspection method
    Cuzzzz the dumps of those structures were totally illegible
    Probably a better way to do this, but IDK what it is,
    so rolling forward with this for now. Hearts for @MichaelBaker
    for bearing through this with me ^_^

--- interpreter/test/ruby/RunTests.hx
+++ interpreter/test/ruby/RunTests.hx
@@ -3,6 +3,7 @@ package ruby;
 class RunTests {
   static function main() {
     var r = new haxe.unit.TestRunner();
+    r.add(new Inspections());
     r.add(new TestLanguageGoBag());
     r.add(new TestInterpreter());
     r.add(new TestParser());
