e54d4284ac7eae68def6fa7a7ba35d3f1c9ec074
Josh Cheek <josh.cheek@gmail.com>
2014-12-17 01:50:34 -0700

    Split parser tests apart again
    Using the web UI is a billion faster than shelling out,
    so split the tests up a bit again. Trying to make it easier
    to understand them, better messages on errors, etc

--- interpreter/test/ruby/TestParser.hx
+++ interpreter/test/ruby/TestParser.hx
@@ -9,87 +9,86 @@ class TestParser extends haxe.unit.TestCase {
                 );
   }
 
-  // because integration tests are so expensive, consolidate them into one large test
+  // literals
+  public function testSpecialObjects() {
+    assertParses(    "nil;    true;    false;    self",
+      AstExpressions([AstNil, AstTrue, AstFalse, AstSelf])
+    );
+  }
+
+  public function testIntegers() {
+    assertParses(               "1;             -123",
+      AstExpressions([AstInteger(1), AstInteger(-123)])
+    );
+  }
+
+  public function testFloat() {
+    assertParses("-12.34", AstFloat(-12.34));
+    // assertParses("1.0", AstFloat(1.0)); // FIXME: it renders `AstFloat(1.0)` to string as `AstFloat(1)`
+  }
+
+  public function testStrings() {
+    assertParses("'abc'", AstString("abc"));
+  }
+
+  // variables
+  public function testVariables() {
+    assertParses("
+      a = 1
+      a
+      A
+      @a = 1
+      @a",
+      AstExpressions([
+        AstSetLocalVariable("a", AstInteger(1)),
+        AstGetLocalVariable("a"),
+        AstConstant(AstNil, "A"), // going w/ nil b/c that's what comes in, but kinda seems like the parser should make this a CurrentNamespace node or something
+        AstSetInstanceVariable("@a", AstInteger(1)),
+        AstGetInstanceVariable("@a"),
+      ])
+    );
+  }
+
+  // sending messages
+  public function testSendingMessages() {
+    assertParses("true.something(false)", AstSend(AstTrue, "something", [AstFalse]));
+  }
+
+  // class and module definitions
+  public function testClassAndModuleDefinitions() {
+    assertParses("
+      class A
+        class B::C < D
+        end
+      end
+      ",
+      AstClass(
+        AstConstant(AstNil, "A"), // name
+        AstNil,                   // superclass
+        AstClass(                 // body
+          AstConstant(AstConstant(AstNil, "B"), "C"),
+          AstConstant(AstNil, "D"),
+          AstNil
+        )
+      )
+    );
+  }
+
+  // method definitions
   public function testAll() {
     assertParses("
-      # literals
-        # special objects
-          nil
-          true
-          false
-          self
-        # Numeric
-          # Integer
-            1
-            -123
-          # Bignum
-          # Float
-            -12.34
-          # Complex
-          # Rational
-        # String
-          'abc'
-      # variables
-        a = 1
-        a
-        A
-        @a = 1
-        @a
-      # sending messages
-        true.something(false)
-      # class/module definitions
-        class A
-          class B::C < D
-          end
-        end
-      # method definitions
-        def bland_method; end
-        def method_with_args_and_body(arg)
-          true
-        end
+      def bland_method; end
+      def method_with_args_and_body(arg)
+        true
+      end
       ",
       AstExpressions([
-        // literals
-          // special objects
-          AstNil,
-          AstTrue,
-          AstFalse,
-          AstSelf,
-          // Numeric
-            // Fixnum
-              AstInteger(1),
-              AstInteger(-123),
-            // Bignum
-            // Float
-              // 1.0 ->  Float(1.0) FIXME: gets cast to Int b/c of confusion on types >.<
-              AstFloat(-12.34),
-            // Complex
-            // Rational
-          // String
-            AstString("abc"),
-        // variables
-          AstSetLocalVariable("a", AstInteger(1)),
-          AstGetLocalVariable("a"),
-          AstConstant(AstNil, "A"), // going w/ nil b/c that's what comes in, but kinda seems like the parser should make this a CurrentNamespace node or something
-          AstSetInstanceVariable("@a", AstInteger(1)),
-          AstGetInstanceVariable("@a"),
-        // sending messages
-          AstSend(AstTrue, "something", [AstFalse]),
-        // class/module definitions
-          AstClass(AstConstant(AstNil, "A"),
-                AstNil,
-                AstClass(AstConstant(AstConstant(AstNil, "B"), "C"),
-                      AstConstant(AstNil, "D"),
-                      AstNil
-                )
-          ),
-        // method definitions
-          AstMethodDefinition("bland_method", [], AstNil),
-          AstMethodDefinition(
-            "method_with_args_and_body",
-            [AstRequiredArg("arg")],
-            AstTrue
-          ),
+        AstMethodDefinition("bland_method", [], AstNil),
+        AstMethodDefinition(
+          "method_with_args_and_body",
+          [AstRequiredArg("arg")],
+          AstTrue
+        ),
       ])
     );
   }
