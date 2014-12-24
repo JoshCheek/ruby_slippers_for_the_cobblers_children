dcbe45d20a9fbe25e4a1582347d679702200d895
Josh Cheek <josh.cheek@gmail.com>
2014-12-23 19:59:03 -0600

    Delete obsolete comments

--- interpreter/src/ruby/Interpreter.hx
+++ interpreter/src/ruby/Interpreter.hx
@@ -118,8 +118,6 @@ class Interpreter {
     case Value(Function(getValue)):
       return Pop(getValue());
 
-    // TODO: Check out guards for pattern matching
-    // TODO: Check empty expression list. It should return nil, pretty sure this returns whatever happens to be the current expression
     case Exprs(Crnt(i, exprs)):
       if(i < exprs.length) return Push(Exprs(Crnt(i+1, exprs)), exprs[i], sf.binding);
       else                 return Pop(currentExpression());
@@ -238,79 +236,4 @@ class Interpreter {
         throw "haven't moved this yet";
     }
   }
-
-
-
-
-
-
-
-  // // updates current evaluation, updates current expression if relevant;
-  // private function setEval(evaluation:EvaluationState):EvaluationState {
-  //   switch(evaluation) {
-  //     case Evaluated(obj):
-  //       world.currentExpression = obj;
-  //       _evaluationFinished = true; // TODO: this name is terrible! there's a Finished value in EvaluationState, but we're actually talking about when an evaluation resolves to an object that a user could see
-  //     case EvaluationList(Cons(crnt, _)):
-  //       setEval(crnt); // child might have finished (FIXME: we're doing 2 responsibilities here, setting eval and setting currentExpression, which is why lines like this are necessary and confusing)
-  //     case SetLocal(_, value):
-  //       setEval(value);
-  //     case _:
-  //       _evaluationFinished = false;
-  //   }
-  //   currentEvaluation = evaluation;
-  //   return evaluation;
-  // }
-
-
-  // private function continueEvaluating(toEval:EvaluationState) {
-  //   switch(toEval) {
-  //     // case Send(Evaluated(target), message):              throw "need to actually do shit now";
-  //     // case Send(target, message):                         return Send(continueEvaluating(target), message);
-  //     case EvaluationList(Cons(Evaluated(obj), ListEnd)): return Evaluated(obj);
-  //     case EvaluationList(Cons(Evaluated(obj), rest)):    return EvaluationList(rest);
-  //     case EvaluationList(Cons(current, rest)):           return EvaluationList(Cons(continueEvaluating(current), rest));
-  //     case GetLocal(name):                                return Evaluated(world.getLocal(name));
-  //     case SetLocal(name, Evaluated(value)):              return Evaluated(world.setLocal(name, value));
-  //     case SetLocal(name, value):                         return SetLocal(name, continueEvaluating(value));
-  //     case Evaluated(_):                                  return Finished;
-  //     case Unevaluated(ast):                              return astToEvaluation(ast);
-  //     case ConstantName(Lookup(ImplicitNamespace, name)):
-  //       // Where is it supposed to look these up? this is one thing I don't fully understand about Ruby :/
-  //       return Evaluated(world.toplevelNamespace.constants[name]);
-
-  //     case ConstantName(Lookup(namespace, name)): throw "multiple namespaces not yet handled!";
-  //     case Finished:                              throw new NothingToEvaluateError("Check before evaluating!");
-
-  //     // fucking ADTS are stupid, even splitting out those values into their own types doesn't prevent stupid shit like this
-  //     case ConstantName(ImplicitNamespace): throw "This shouldn't be possible!";
-  //     case EvaluationList(ListEnd):         throw "This shouldn't be possible!";
-  //   }
-  // }
-
-  // private function astToEvaluation(ast:Ast):EvaluationState {
-  //   switch(ast) {
-  //     case AstSend(target, message, args): return Evaluated(world.stringLiteral("SEND NOT ACTUALL IMPLEMENTED")); // Send(astToEvaluation(target), message);
-  //     case AstConstant(namespace, name):   // FIXME: not handling namespace properly
-  //                                          return ConstantName(Lookup(ImplicitNamespace, name));
-  //     case AstFalse:                       return Evaluated(world.rubyFalse);
-  //     case AstTrue:                        return Evaluated(world.rubyTrue);
-  //     case AstNil:                         return Evaluated(world.rubyNil);
-  //     case AstString(value):               return Evaluated(world.stringLiteral(value));
-  //     case AstExpressions(exprs):
-  //       return
-  //         if(exprs.length == 0)      Evaluated(world.rubyNil);
-  //         else if(exprs.length == 1) EvaluationList(Cons(astToEvaluation(exprs[0]), ListEnd));
-  //         else                       EvaluationList(exprs
-  //                                      .fromEnd()
-  //                                      .fold(
-  //                                        function(el, lst) return Cons(astToEvaluation(el), lst),
-  //                                        ListEnd
-  //                                      ));
-  //     case AstGetLocalVariable(name):        return GetLocal(name);
-  //     case AstSetLocalVariable(name, value): return SetLocal(name, astToEvaluation(value));
-  //     case _:
-  //       throw "Unhandled: " + ast;
-  //   }
-  // }
 }
