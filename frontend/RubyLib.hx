b5e8213537ee5d29a6b0936b923c13f2f19f1ea8
Josh Cheek <josh.cheek@gmail.com>
2015-01-01 00:34:55 -0700

    IDK, just committing
    Was sitting around my repo waiting for HTTP requests to work in node,
    but they aren't going to, b/c node is built on a horrible langauge,
    so going to just commit them to get them out of my working dir

--- frontend/RubyLib.hx
+++ frontend/RubyLib.hx
@@ -2,23 +2,74 @@
 // haxe -main RubyLib -cp src -js RubyLib.js
 // env NODE_PATH=. node run.js
 
-import ruby.*;
-import ruby.ds.*;
+import Stack;
+import ruby.ds.Errors;
+import ruby.ds.InternalMap;
+import ruby.ds.Interpreter;
+import ruby.ds.Objects;
+import ruby.ds.World;
+import ruby.Bootstrap;
+import ruby.Core;
+import ruby.Http;
+import ruby.Interpreter;
+import ruby.LanguageGoBag;
+import ruby.ParseRuby;
+import ruby.World;
 
-@:native("module") extern class NodeModule { public static var exports:Dynamic; }
-@:native("ruby")   extern class RubyNamespace { }
+// @:native("module") extern class NodeModule { public static var exports:Dynamic; }
+// @:native("ruby")   extern class RubyNamespace { }
 
+// Note that the ruby namespace is exported, not this class!!
 class RubyLib {
   // don't delete my code just b/c I'm not using it... this is a library!
   // why doesn't `-dce no` do this? :(
-  @:keep static function noDce() {
+  @:keep static public function noDce(rawCode:String) {
+    // var worldDs     = ruby.Bootstrap.bootstrap();
+    // var world       = new ruby.World(worldDs);
+    // var interpreter = world.interpreter;
+    // var ast         = ruby.ParseRuby.fromCode(code);
+    // Ruby.
+    // interpreter.pushCode(ast);
+    // trace(world.inspect(interpreter.currentExpression));
     var worldDs     = ruby.Bootstrap.bootstrap();
     var world       = new ruby.World(worldDs);
     var interpreter = world.interpreter;
-    trace(world.inspect(interpreter.currentExpression)); // #<NilClass>
+    var ast         = ruby.ParseRuby.fromCode(rawCode);
+
+    interpreter.pushCode(ast);
+
+    trace("CODE TO INTERPRET: \n" + rawCode);
+    trace("--------------------");
+
+    while(interpreter.isInProgress) {
+      trace(world.inspect(interpreter.currentExpression));
+      interpreter.nextExpression();
+    }
+    trace("--------------------");
+    trace("PRINTED: " + world.printedToStdout);
   }
 
   public static function main() {
-    NodeModule.exports = RubyNamespace;
+#if js
+    untyped __js__("module.exports = ruby;");
+    untyped __js__("ruby.RubyLib = RubyLib;");
+#end
+    var rawCode = 'class User\n' +
+                  '  def initialize(name)\n' +
+                  '    self.name = name\n' +
+                  '  end\n' +
+                  '\n'+
+                  '  def name\n' +
+                  '    @name\n' +
+                  '  end\n' +
+                  '\n'+
+                  '  def name=(name)\n' +
+                  '    @name = name\n' +
+                  '  end\n' +
+                  'end\n' +
+                  '\n' +
+                  'user = User.new("Josh")\n' +
+                  'puts user.name';
+    noDce(rawCode);
   }
 }
--- frontend/run.js
+++ frontend/run.js
@@ -3,10 +3,37 @@
 // but still, means that this stuff tends to explode b/c I defined it, also, but not for that purpose,
 // and it explodes unhelpfully b/c they call it with the wrong number of args, so param is null -.-
 (function() {
-var ruby              = require("RubyLib");
-var worldDs           = ruby.Bootstrap.bootstrap();
-var world             = new ruby.World(worldDs);
-var interpreter       = world.interpreter;
-var currentExpression = interpreter.get_currentExpression() // b/c the currentExpression property is abstract, this is the underlying implementation
-console.log(world.inspect(currentExpression)); // #<NilClass>
+  // var rawCode = 'class User\n' +
+  //               '  def initialize(name)\n' +
+  //               '    self.name = name\n' +
+  //               '  end\n' +
+  //               '\n'+
+  //               '  def name\n' +
+  //               '    @name\n' +
+  //               '  end\n' +
+  //               '\n'+
+  //               '  def name=(name)\n' +
+  //               '    @name = name\n' +
+  //               '  end\n' +
+  //               'end\n' +
+  //               '\n' +
+  //               'user = User.new("Josh")\n' +
+  //               'puts user.name';
+  var ruby        = require("RubyLib");
+  // var worldDs     = ruby.Bootstrap.bootstrap();
+  // var world       = new ruby.World(worldDs);
+  // var interpreter = world.interpreter;
+  // var ast         = ruby.ParseRuby.fromCode(rawCode);
+
+  // interpreter.pushCode(ast);
+
+  // console.log("CODE TO INTERPRET: \n" + rawCode);
+  // console.log("--------------------");
+
+  // // while(interpreter.get_isInProgress()) {
+  //   console.log(world.inspect(interpreter.get_currentExpression()));
+  //   interpreter.nextExpression();
+  // // }
+  // console.log("--------------------");
+  // console.log("PRINTED: " + world.printedToStdout);
 })();
