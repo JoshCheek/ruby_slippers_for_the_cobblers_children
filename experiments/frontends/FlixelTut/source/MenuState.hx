3a339815052b621598de65438377eb9f201bd7d6
Josh Cheek <josh.cheek@gmail.com>
2014-12-31 21:11:06 -0700

    Tutorial: add the next line they specify, switch tabs to spaces, clean up comments

--- experiments/frontends/FlixelTut/source/MenuState.hx
+++ experiments/frontends/FlixelTut/source/MenuState.hx
@@ -7,29 +7,21 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 
-/**
- * A FlxState which can be used for the game's menu.
- */
+// A FlxState which can be used for the game's menu.
 class MenuState extends FlxState {
-	/**
-	 * Function that is called up when to state is created to set it up.
-	 */
-	override public function create():Void {
-		super.create();
-	}
+  // called to initialize a new "to state"
+  override public function create():Void {
+    super.create();
+  }
 
-	/**
-	 * Function that is called when this state is destroyed - you might want to
-	 * consider setting all objects this state uses to null to help garbage collection.
-	 */
-	override public function destroy():Void {
-		super.destroy();
-	}
+  // called when this state is destroyed
+  override public function destroy():Void {
+    super.destroy();
+  }
 
-	/**
-	 * Function that is called once every frame.
-	 */
-	override public function update():Void {
-		super.update();
-	}
+  // called once every frame.
+  override public function update():Void {
+    add(new FlxText(10, 10, 100, "Hello, World!"));
+    super.update();
+  }
 }
