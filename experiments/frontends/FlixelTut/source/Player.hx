89f0eefc1dbb3210a0c23a66c75280bb0735b127
Josh Cheek <josh.cheek@gmail.com>
2015-01-02 16:16:40 -0700

    *sigh* my version of the tutorial breaks
    The assets thing takes a Dynamic so it doesn't get typechecked.
    It ultimately blows up here: https://github.com/HaxeFlixel/flixel/blob/3.3.6/flixel/FlxSprite.hx#L807
    but I couldn't figure out why. Tried their Project.xml and their Player.hx
    Going to just steal their whole implementation and skim the rest of the tut
    so I can move on

--- experiments/frontends/FlixelTut/source/Player.hx
+++ experiments/frontends/FlixelTut/source/Player.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.util.FlxAngle;
@@ -10,14 +11,42 @@ class Player extends FlxSprite {
 
   public function new(x:Float=0, y:Float=0) {
     super(x, y);
-    makeGraphic(16, 16, FlxColor.BLUE);
-    drag.x = drag.y = 1600; // friction
+
+    // loadGraphic("assets/images/player.png", true, 16, 16);
+    // loadGraphic(AssetPaths.player__png, true, 16, 16);
+    loadGraphic("assets/images/player.png", true, 16, 16);
+
+    setFacingFlip(FlxObject.LEFT, false, false);
+    setFacingFlip(FlxObject.RIGHT, true, false);
+
+    // animation.FlxAnimationController#add(
+    //   Name:String, Frames:Array<Int>, FrameRate:Int = 30, Looped:Bool = true
+    // ):Void
+    animation.add("d",  [0, 1, 0, 2], 6, false);
+    animation.add("lr", [3, 4, 3, 5], 6, false);
+    animation.add("u",  [6, 7, 6, 8], 6, false);
+
+    width    = 8;
+    height   = 14;
+    offset.x = 4;
+    offset.y = 2;
   }
 
 
   override public function update():Void {
     movement();
     super.update();
+  }
+
+  override public function draw():Void {
+    if(velocity.x != 0 || velocity.y != 0) {
+      switch(facing) {
+        case FlxObject.LEFT, FlxObject.RIGHT: animation.play("lr");
+        case FlxObject.UP:                    animation.play("u");
+        case FlxObject.DOWN:                  animation.play("d");
+      }
+    }
+    super.draw();
   }
 
   private function movement():Void {
@@ -42,10 +71,20 @@ class Player extends FlxSprite {
         else if (_down)            90;
         else if (_left)           180;
         else                        0;
+
+      // an int at least 2 bytes long (these are defined as 0x1000, depending which "bit" they set decides the direction)
+      // not sure why they do this instead of using Enumerables, or 4 bits instead of 2 bytes
+      facing = _up    ? FlxObject.UP    :
+               _down  ? FlxObject.DOWN  :
+               _left  ? FlxObject.LEFT  :
+               _right ? FlxObject.RIGHT :
+                        FlxObject.RIGHT;
+
       // NOTE: on 4.0, this becomes FlxPoint#rotate,
       // and the direction of the angle isn't inverted
       // (I'm on 3.3.6)
       FlxAngle.rotatePoint(speed, 0, 0, 0, mA, velocity);
     }
   }
+
 }
