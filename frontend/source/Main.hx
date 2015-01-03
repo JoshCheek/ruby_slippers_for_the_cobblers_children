c91aee7bc485983f73e6f4aeea5e5591b7295ac6
Josh Cheek <josh.cheek@gmail.com>
2015-01-03 00:13:44 -0700

    Refactor generated code
    Delete classes it doesn't use, shorten/delete comments,
    consolidate methods that I don't understand why they're split up,
    make syntax less annoying (lowercase var names, braces don't need
    an entire line to themselves, etc)

--- frontend/source/Main.hx
+++ frontend/source/Main.hx
@@ -8,61 +8,35 @@ import flash.Lib;
 import flixel.FlxGame;
 import flixel.FlxState;
 
-class Main extends Sprite 
-{
-	var gameWidth:Int = 640; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var gameHeight:Int = 480; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = MenuState; // The FlxState the game starts with.
-	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 60; // How many frames per second the game should run at.
-	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
-	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-	
-	// You can pretty much ignore everything from here on - your code should go in your states.
-	
-	public static function main():Void
-	{	
-		Lib.current.addChild(new Main());
+class Main extends Sprite {
+	public static function main():Void {
+		Lib.current.addChild(new Main()); // fkn global state everywhere :(
 	}
-	
-	public function new() 
-	{
+
+	public function new() {
 		super();
-		
-		if (stage != null) 
-		{
-			init();
-		}
-		else 
-		{
-			addEventListener(Event.ADDED_TO_STAGE, init);
-		}
+    if(stage == null) addEventListener(Event.ADDED_TO_STAGE, init);
+    else              init();
 	}
-	
-	private function init(?E:Event):Void 
-	{
-		if (hasEventListener(Event.ADDED_TO_STAGE))
-		{
-			removeEventListener(Event.ADDED_TO_STAGE, init);
-		}
-		
-		setupGame();
-	}
-	
-	private function setupGame():Void
-	{
-		var stageWidth:Int = Lib.current.stage.stageWidth;
-		var stageHeight:Int = Lib.current.stage.stageHeight;
 
-		if (zoom == -1)
-		{
-			var ratioX:Float = stageWidth / gameWidth;
-			var ratioY:Float = stageHeight / gameHeight;
-			zoom = Math.min(ratioX, ratioY);
-			gameWidth = Math.ceil(stageWidth / zoom);
-			gameHeight = Math.ceil(stageHeight / zoom);
-		}
+	private function init(?e:Event):Void {
+    if (hasEventListener(Event.ADDED_TO_STAGE))
+      removeEventListener(Event.ADDED_TO_STAGE, init);
 
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+    var gameWidth       : Int             = 640;       // might change, depending on your zoom.
+    var gameHeight      : Int             = 480;       // might change, depending on your zoom.
+    var updateFps       : Int             = 60;        // how often to call update
+    var drawFps         : Int             = 60;        // how often to call draw
+    var skipSplash      : Bool            = true;
+    var startFullscreen : Bool            = false;
+    var initialState    : Class<FlxState> = MenuState;
+		var stageWidth      : Int             = Lib.current.stage.stageWidth;
+		var stageHeight     : Int             = Lib.current.stage.stageHeight;
+    var zoom            : Float           = Math.min(stageWidth/gameWidth, stageHeight/gameHeight);
+
+    gameWidth  = Math.ceil(stageWidth  / zoom);
+    gameHeight = Math.ceil(stageHeight / zoom);
+
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, updateFps, drawFps, skipSplash, startFullscreen));
 	}
 }
--- frontend/source/MenuState.hx
+++ frontend/source/MenuState.hx
@@ -7,33 +7,22 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 
-/**
- * A FlxState which can be used for the game's menu.
- */
+// A FlxState which can be used for the game's menu.
 class MenuState extends FlxState
 {
-	/**
-	 * Function that is called up when to state is created to set it up. 
-	 */
-	override public function create():Void
-	{
+	// called when to state is created (to set it up)
+	override public function create():Void {
+    add(new FlxText(10, 10, 100, "Hello, World!"));
 		super.create();
 	}
-	
-	/**
-	 * Function that is called when this state is destroyed - you might want to 
-	 * consider setting all objects this state uses to null to help garbage collection.
-	 */
-	override public function destroy():Void
-	{
+
+	// called when this state is destroyed
+	override public function destroy():Void {
 		super.destroy();
 	}
 
-	/**
-	 * Function that is called once every frame.
-	 */
-	override public function update():Void
-	{
+	// called once every frame
+	override public function update():Void {
 		super.update();
-	}	
+	}
 }
