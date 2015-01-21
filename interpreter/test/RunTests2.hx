928280c5aa85703d597f1f5d3354f4a2e0948686
Josh Cheek <josh.cheek@gmail.com>
2015-01-21 11:17:37 -0700

    SpaceCadet: Clean up runner a bit

--- interpreter/test/RunTests2.hx
+++ interpreter/test/RunTests2.hx
@@ -1,16 +1,16 @@
-import spaceCadet.SpaceCadet;
+import spaceCadet.*;
 
 class RunTests2 {
   static function main() {
     // define tests
-    var root = new spaceCadet.SpaceCadet.Description();
-    spaceCadet.DescribeRunningASuite.describe(root);
-    spaceCadet.DescribeAssertions.describe(root);
-    spaceCadet.DescribeBeforeBlocks.describe(root);
+    var root = new SpaceCadet.Description();
+    DescribeRunningASuite.describe(root);
+    DescribeAssertions.describe(root);
+    DescribeBeforeBlocks.describe(root);
 
     // run and report
-    var output   = new spaceCadet.SpaceCadet.Output(Sys.stdout(), Sys.stderr());
-    var reporter = new spaceCadet.SpaceCadet.StreamReporter(output);
+    var output   = new Output(Sys.stdout(), Sys.stderr());
+    var reporter = new SpaceCadet.StreamReporter(output);
     spaceCadet.SpaceCadet.Run.run(root, reporter);
 
     if(reporter.numFails != 0) Sys.exit(1);
