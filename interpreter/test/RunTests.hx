291b1c777cc74a906f623c71ef1451e7b6a8bdea
Josh Cheek <josh.cheek@gmail.com>
2015-01-21 14:17:56 -0700

    Consolidate test runners

--- interpreter/test/RunTests.hx
+++ interpreter/test/RunTests.hx
@@ -2,22 +2,27 @@ import spaceCadet.*;
 
 class RunTests {
   static function main() {
-    // haxe.unit tests
-    var runner = new haxe.unit.TestRunner();
-    toplevel.RunTests.addTests(runner);
-    ruby.RunTests.addTests(runner);
-    var haxeUnitPassed = runner.run();
+    // haxe.unit
+      var runner = new haxe.unit.TestRunner();
+      toplevel.RunTests.addTests(runner);
+      ruby.RunTests.addTests(runner);
+      var haxeUnitPassed = runner.run();
 
-    // SpaceCadet tests
-    var root = new Description();
-    toplevel.DescribeStringOutput.describe(root);
-    var output   = new Output(Sys.stdout(), Sys.stderr());
-    var reporter = new Reporter.StreamReporter(output);
-    Run.run(root, reporter);
-    var spaceCadetPassed = reporter.numFails == 0;
+    // SpaceCadet
+      // define
+      var root = new Description();
+      spaceCadet.DescribeRunningASuite.describe(root);
+      spaceCadet.DescribeAssertions.describe(root);
+      spaceCadet.DescribeBeforeBlocks.describe(root);
+      toplevel.DescribeStringOutput.describe(root);
+      // run
+      var output   = new Output(Sys.stdout(), Sys.stderr());
+      var reporter = new Reporter.StreamReporter(output);
+      Run.run(root, reporter);
+      var spaceCadetPassed = reporter.numFails == 0;
 
-    // exiting
-    var hadFailure = !haxeUnitPassed || !spaceCadetPassed;
-    if(hadFailure) Sys.exit(1);
+    // Exit status
+      var hadFailure = !haxeUnitPassed || !spaceCadetPassed;
+      if(hadFailure) Sys.exit(1);
   }
 }
