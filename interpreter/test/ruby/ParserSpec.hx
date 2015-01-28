33be39c68daaf9e059903a458103d6430256db69
Josh Cheek <josh.cheek@gmail.com>
2015-01-28 13:45:36 -0700

    Interpreter: ParserSpec - more tests and more isolated
    Parsing via server is much faster than shelling out,
    so we don't need to put all the contexts into a few giant tests.
    Instead, split out as much as is reasonable.

--- interpreter/test/ruby/ParserSpec.hx
+++ interpreter/test/ruby/ParserSpec.hx
@@ -9,88 +9,109 @@ class ParserSpec {
     a.eq(expected.inspect(), actual.inspect(), c);
   }
 
+  public static function exprs(exprs) {
+    return Exprs(Start(exprs));
+  }
+
   public static function describe(d:spaceCadet.Description) {
-    d.describe('literals', function(d) {
-      d.example('special objects', function(a) {
-        parses(a, "nil; true; false; self",
-                  Exprs(Start([Nil, True, False, Self])));
+    d.describe('Parsing', function(d) {
+      d.describe('literals', function(d) {
+        d.example('nil',   function(a) parses(a, 'nil',   Nil));
+        d.example('true',  function(a) parses(a, 'true',  True));
+        d.example('false', function(a) parses(a, 'false', False));
+        d.example('self',  function(a) parses(a, 'self',  Self));
+        d.example('integers', function(a) {
+          parses(a, "1",    Integer(1));
+          parses(a, "-123", Integer(-123));
+        });
+        d.example('float', function(a) {
+          parses(a, "-12.34", Float(-12.34));
+          // parses(a, "1.0", Float(1.0)); // FIXME: it renders `Float(1.0)` to string as `Float(1)`
+        });
+        d.example('string', function(a) {
+          parses(a, "'abc'", String("abc"));
+        });
       });
-      d.example('integers', function(a) {
-        parses(a,"1; -123",
-                 Exprs(Start([Integer(1), Integer(-123)])));
-      });
-      d.example('float', function(a) {
-        parses(a, "-12.34", Float(-12.34));
-        // parses(a, "1.0", Float(1.0)); // FIXME: it renders `Float(1.0)` to string as `Float(1)`
-      });
-      d.example('string', function(a) {
-        parses(a, "'abc'", String("abc"));
-      });
-    });
 
-    d.describe('variables', function(d) {
-      d.it('parses them :P', function(a) {
-        parses(a, "
-          a = 1
-          a
-          A
-          @a = 1
-          @a",
-          Exprs(Start([
-            SetLvar(FindRhs("a", Integer(1))),
-            GetLvar(Name("a")),
-            Const(GetNs(Default, "A")), // going w/ nil b/c that's what comes in, but kinda seems like the parser should make this a CurrentNamespace node or something
-            SetIvar(FindRhs("@a", Integer(1))),
-            GetIvar(Name("@a")),
-          ]))
-        );
+      d.describe('variables', function(d) {
+        d.context('Constant', function(a) {
+          d.example('with no namespace', function(a) {
+            parses(a, "A", Const(GetNs(Default, "A")));
+          });
+          d.example('with a namespace', function(a) {
+            parses(a, "A::B", Const(GetNs(Const(GetNs(Default, "A")), "B")));
+          });
+        });
+        d.it('setting and getting local vars', function(a) {
+          parses(a, "a = 1; a",
+            exprs( [SetLvar(FindRhs("a", Integer(1))),
+                    GetLvar(Name("a"))]));
+        });
+        d.it('setting and getting instance vars', function(a) {
+          parses(a, "@a = 1; @a",
+            exprs( [SetIvar(FindRhs("@a", Integer(1))),
+                    GetIvar(Name("@a"))]));
+        });
       });
-    });
 
-    d.describe('sending messages', function(d) {
-      d.it('parses the target, message, and arguments', function(a) {
-        parses(a, "true.something(false)", Send(Start(True, "something", [False])));
+      d.describe('sending messages', function(d) {
+        d.it('parses the target, message, and arguments', function(a) {
+          parses(a, "true.something(false)", Send(Start(True, "something", [False])));
+        });
       });
-    });
 
-    d.describe('class and module definitions', function(d) {
-      d.it('parses lots of stuff...', function(a) {
-        parses(a, "
-          class A
-            class B::C < D
-            end
-          end
-          ",
-          OpenClass(GetNs(
-            Const(GetNs(Default, "A")), // name
-            Default,                    // superclass
-            OpenClass(GetNs(            // body
-              Const(GetNs(Const(GetNs(Default, "B")), "C")),
-              Const(GetNs(Default, "D")),
-              Default
+      d.describe('class definitions', function(d) {
+        d.it('parses the namespace, name, superclas, and body', function(a) {
+          parses(a, "class A::B < C
+                       1
+                     end",
+             OpenClass(GetNs(
+               Const(GetNs(  Const(GetNs(Default, "A")), "B")),
+               Const(GetNs(Default, "C")),
+               Integer(1)
+             )));
+        });
+
+        d.it('parses defaults there is no namespace, superclass, or body', function(a) {
+          parses(a, "class A; end", OpenClass(GetNs(
+                                      Const(GetNs(Default, "A")),
+                                      Default,
+                                      Default
+                                    )));
+        });
+
+        d.it('parses nested declarations', function(a) {
+          parses(a, "class A; class B; end; end",
+            OpenClass(GetNs(
+              Const(GetNs(Default, "A")),
+              Default,
+              OpenClass(GetNs(
+                Const(GetNs(Default, "B")),
+                Default,
+                Default
+              ))
             ))
-          ))
-        );
+          );
+        });
       });
-    });
 
-    d.describe('method definitions', function(d) {
-      d.it('parses them', function(a) {
-        parses(a, "
-          def bland_method; end
-          def method_with_args_and_body(req, *rst)
-            true
-          end
-          ",
-          Exprs(Start([
-            Def(Start("bland_method", [], Default)),
-            Def(Start(
-              "method_with_args_and_body",
-              [Required("req"), Rest("rst")],
-              True
-            )),
-          ]))
-        );
+      d.describe('module definitions', function(d) {
+      });
+
+      d.describe('method definitions', function(d) {
+        d.example('with no args or body', function(a) {
+          parses(a, "def bland_method; end", Def(Start("bland_method", [], Default)));
+        });
+        d.example('with a body', function(a) {
+          parses(a, "def hasbody; true; end", Def(Start("hasbody", [], True)));
+        });
+        d.example('with required and rest args', function(a) {
+          parses(a, "def hasargs(req, *rst); end",
+              Def(Start("hasargs",
+                [Required("req"), Rest("rst")],
+                Default
+              )));
+        });
       });
     });
   }
