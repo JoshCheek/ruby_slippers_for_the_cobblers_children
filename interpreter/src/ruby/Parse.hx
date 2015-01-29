15e4cec642d2331cf1ba8e89b0710dec0cf31bfd
Josh Cheek <josh.cheek@gmail.com>
2015-01-29 14:00:34 -0700

    Interpreter: Begin switching to AST with objs instead of enums

---
+++ interpreter/src/ruby/Parse.hx
@@ -0,0 +1,236 @@
+package ruby;
+
+typedef Location = {
+  public var begin : Int;
+  public var end   : Int;
+}
+
+typedef AstAttributes = { }
+class Ast {
+  public function new(?attributes:AstAttributes) {
+  }
+
+  public var isDefault   (get, never) : Bool;
+  public var isNil       (get, never) : Bool;
+  public var isSelf      (get, never) : Bool;
+  public var isTrue      (get, never) : Bool;
+  public var isFalse     (get, never) : Bool;
+  public var isInteger   (get, never) : Bool;
+  public var isFloat     (get, never) : Bool;
+  public var isString    (get, never) : Bool;
+  public var isGetLvar   (get, never) : Bool;
+  public var isSetLvar   (get, never) : Bool;
+  public var isGetIvar   (get, never) : Bool;
+  public var isSetIvar   (get, never) : Bool;
+  public var isConst     (get, never) : Bool;
+  public var isExprs     (get, never) : Bool;
+  public var isOpenClass (get, never) : Bool;
+  public var isSend      (get, never) : Bool;
+  public var isValue     (get, never) : Bool;
+  public var isDef       (get, never) : Bool;
+
+  public function get_isDefault()   return false;
+  public function get_isNil()       return false;
+  public function get_isSelf()      return false;
+  public function get_isTrue()      return false;
+  public function get_isFalse()     return false;
+  public function get_isInteger()   return false;
+  public function get_isFloat()     return false;
+  public function get_isString()    return false;
+  public function get_isGetLvar()   return false;
+  public function get_isSetLvar()   return false;
+  public function get_isGetIvar()   return false;
+  public function get_isSetIvar()   return false;
+  public function get_isConst()     return false;
+  public function get_isExprs()     return false;
+  public function get_isOpenClass() return false;
+  public function get_isSend()      return false;
+  public function get_isValue()     return false;
+  public function get_isDef()       return false;
+
+  public function toDefault()   : DefaultAst   { throw("INVALID!"); return null; }
+  public function toNil()       : NilAst       { throw("INVALID!"); return null; }
+  public function toSelf()      : SelfAst      { throw("INVALID!"); return null; }
+  public function toTrue()      : TrueAst      { throw("INVALID!"); return null; }
+  public function toFalse()     : FalseAst     { throw("INVALID!"); return null; }
+  public function toInteger()   : IntegerAst   { throw("INVALID!"); return null; }
+  public function toFloat()     : FloatAst     { throw("INVALID!"); return null; }
+  public function toString()    : StringAst    { throw("INVALID!"); return null; }
+  public function toGetLvar()   : GetLvarAst   { throw("INVALID!"); return null; }
+  public function toSetLvar()   : SetLvarAst   { throw("INVALID!"); return null; }
+  public function toGetIvar()   : GetIvarAst   { throw("INVALID!"); return null; }
+  public function toSetIvar()   : SetIvarAst   { throw("INVALID!"); return null; }
+  public function toConst()     : ConstAst     { throw("INVALID!"); return null; }
+  public function toExprs()     : ExprsAst     { throw("INVALID!"); return null; }
+  public function toOpenClass() : OpenClassAst { throw("INVALID!"); return null; }
+  public function toSend()      : SendAst      { throw("INVALID!"); return null; }
+  public function toValue()     : ValueAst     { throw("INVALID!"); return null; }
+  public function toDef()       : DefAst       { throw("INVALID!"); return null; }
+}
+
+class DefaultAst extends Ast {
+  override public function get_isDefault() return true;
+  override public function toDefault() return this;
+}
+
+class NilAst extends Ast {
+  override public function get_isNil() return true;
+  override public function toNil() return this;
+}
+
+class SelfAst extends Ast {
+  override public function get_isSelf() return true;
+  override public function toSelf() return this;
+}
+
+class TrueAst extends Ast {
+  override public function get_isTrue() return true;
+  override public function toTrue() return this;
+}
+
+class FalseAst extends Ast {
+  override public function get_isFalse() return true;
+  override public function toFalse() return this;
+}
+
+typedef IntegerAstAttributes = {
+  > AstAttributes,
+  var value:Int;
+}
+class IntegerAst extends Ast {
+  public var value:Int;
+  public function new(attributes:IntegerAstAttributes) {
+    this.value = attributes.value;
+    super(attributes);
+  }
+  override public function get_isInteger() return true;
+  override public function toInteger() return this;
+}
+
+typedef FloatAstAttributes = {
+  > AstAttributes,
+  var value:Float;
+}
+class FloatAst extends Ast {
+  public var value:Float;
+  public function new(attributes:FloatAstAttributes) {
+    this.value = attributes.value;
+    super(attributes);
+  }
+  override public function get_isFloat() return true;
+  override public function toFloat() return this;
+}
+
+typedef StringAstAttributes = {
+  > AstAttributes,
+  var value:String;
+}
+class StringAst extends Ast {
+  public var value:String;
+  public function new(attributes:StringAstAttributes) {
+    this.value = attributes.value;
+    super(attributes);
+  }
+  override public function get_isString() return true;
+  override public function toString() return this;
+}
+
+class GetLvarAst extends Ast {
+  override public function get_isGetLvar() return true;
+  override public function toGetLvar() return this;
+}
+
+class SetLvarAst extends Ast {
+  override public function get_isSetLvar() return true;
+  override public function toSetLvar() return this;
+}
+
+class GetIvarAst extends Ast {
+  override public function get_isGetIvar() return true;
+  override public function toGetIvar() return this;
+}
+
+class SetIvarAst extends Ast {
+  override public function get_isSetIvar() return true;
+  override public function toSetIvar() return this;
+}
+
+class ConstAst extends Ast {
+  override public function get_isConst() return true;
+  override public function toConst() return this;
+}
+
+class ExprsAst extends Ast {
+  override public function get_isExprs() return true;
+  override public function toExprs() return this;
+}
+
+class OpenClassAst extends Ast {
+  override public function get_isOpenClass() return true;
+  override public function toOpenClass() return this;
+}
+
+class SendAst extends Ast {
+  override public function get_isSend() return true;
+  override public function toSend() return this;
+}
+
+class ValueAst extends Ast {
+  override public function get_isValue() return true;
+  override public function toValue() return this;
+}
+
+class DefAst extends Ast {
+  override public function get_isDef() return true;
+  override public function toDef() return this;
+}
+
+
+class Parse {
+  public static var serverUrl = 'http://localhost:3003';
+
+  public static function fromString(rawCode:String, ?serverUrl:String):Ast {
+    if(serverUrl == null) serverUrl = Parse.serverUrl;
+    var jsonString = ruby.Http.post(serverUrl, rawCode);
+    var json       = haxe.Json.parse(jsonString);
+    return fromJson(json);
+  }
+
+  static function fromJson(ast:Dynamic):Ast {
+    if(ast == null) return new DefaultAst({});
+    return switch(ast.type) {
+      case "nil"                   : new NilAst();
+      case "true"                  : new TrueAst();
+      case "false"                 : new FalseAst();
+      case "self"                  : new SelfAst();
+      case "integer"               : new IntegerAst({value: Std.parseInt(ast.value)});
+      case "float"                 : new FloatAst({value: Std.parseFloat(ast.value)});
+      case "string"                : new StringAst({value: ast.value});
+      // case "expressions"           : Exprs(Start(ast.expressions.map(fromJson)));
+      // case "set_local_variable"    : SetLvar(FindRhs(ast.name, fromJson(ast.value)));
+      // case "get_local_variable"    : GetLvar(Name(ast.name));
+      // case "set_instance_variable" : SetIvar(FindRhs(ast.name, fromJson(ast.value)));
+      // case "get_instance_variable" : GetIvar(Name(ast.name));
+      // case "send"                  : Send(Start(fromJson(ast.target), ast.message, ast.args.map(fromJson)));
+      // case "constant"              : Const(GetNs(fromJson(ast.namespace), ast.name));
+      // case "class"                 : OpenClass(GetNs(fromJson(ast.name_lookup), fromJson(ast.superclass), fromJson(ast.body)));
+      // case "method_definition"     : Def(Start(ast.name, ast.args.map(toArg), fromJson(ast.body)));
+      case _                       : throw("CAN'T PARSE: " + ast);
+    }
+  }
+
+  // private static function toArg(arg:Dynamic):ArgType {
+  //   switch(arg.type) {
+  //     case "required_arg": return Required(arg.name);
+  //     case "rest_arg":     return Rest(arg.name);
+  //     case _: throw("Unknown arg type!: " + arg);
+  //   }
+  // }
+
+  // private static function locationFrom(ast:Dynamic) {
+  //   return {
+  //     begin: ast.location.begin,
+  //     end:   ast.location.end,
+  //   };
+  // }
+}
---
+++ interpreter/test/ruby/ParseSpec.hx
@@ -0,0 +1,153 @@
+package ruby;
+using Inspect;
+
+class ParseSpec {
+  public static function parse(rubyCode:String) {
+    return ruby.Parse.fromString(rubyCode);
+  }
+
+  // public static function exprs(exprs) {
+  //   return Exprs(Start(exprs));
+  // }
+
+  public static function describe(d:spaceCadet.Description) {
+    var parsed:ruby.Parse.Ast;
+    d.before(function(a) parsed = null);
+
+    d.describe('Parsing', function(d) {
+      d.describe('literals', function(d) {
+        d.example('nil',   function(a) {
+          parsed = parse('nil');
+          a.isTrue(parsed.isNil);
+          parsed.toNil();
+        });
+        d.example('true',  function(a) {
+          parsed = parse('true');
+          a.isTrue(parsed.isTrue);
+          parsed.toTrue();
+        });
+        d.example('false', function(a) {
+          parsed = parse('false');
+          a.isTrue(parsed.isFalse);
+          parsed.toFalse();
+        });
+        d.example('self',  function(a) {
+          parsed = parse('self');
+          a.isTrue(parsed.isSelf);
+          parsed.toSelf();
+        });
+        d.example('integers', function(a) {
+          parsed = parse('1');
+          a.isTrue(parsed.isInteger);
+          a.eq(1, parsed.toInteger().value);
+
+          parsed = parse('-123');
+          a.isTrue(parsed.isInteger);
+          a.eq(-123, parsed.toInteger().value);
+        });
+        d.example('float', function(a) {
+          parsed = parse('-12.34');
+          a.isTrue(parsed.isFloat);
+          a.eq(-12.34, parsed.toFloat().value);
+
+          parsed = parse('1.0');
+          a.isTrue(parsed.isFloat);
+          a.eq(1.0, parsed.toFloat().value);
+        });
+        d.example('string', function(a) {
+          parsed = parse('"abc"');
+          a.isTrue(parsed.isString);
+          a.eq("abc", parsed.toString().value);
+        });
+      });
+
+      // d.describe('variables', function(d) {
+      //   d.context('Constant', function(a) {
+      //     d.example('with no namespace', function(a) {
+      //       parsed = parse("A").toConst;
+      //       a.eq("A", parsed.name);
+      //       a.eq(true, parsed.ns.ns.isDefault);
+      //     });
+      //     d.example('with a namespace', function(a) {
+      //       parsed = parse("A::B").toConst;
+      //       a.eq("B", parsed.name);
+      //       a.eq("A", parsed.ns.name);
+      //       a.eq(true, parsed.ns.ns.isDefault);
+      //     });
+      //   });
+      //   d.it('setting and getting local vars', function(a) {
+      //     parses(a, "a = 1; a",
+      //       exprs( [SetLvar(FindRhs("a", Integer(1))),
+      //               GetLvar(Name("a"))]));
+      //   });
+      //   d.it('setting and getting instance vars', function(a) {
+      //     parses(a, "@a = 1; @a",
+      //       exprs( [SetIvar(FindRhs("@a", Integer(1))),
+      //               GetIvar(Name("@a"))]));
+      //   });
+      // });
+
+      // d.describe('sending messages', function(d) {
+      //   d.it('parses the target, message, and arguments', function(a) {
+      //     parses(a, "true.something(false)", Send(Start(True({begin:0, end:4}),
+      //                                                   "something",
+      //                                                   [False])));
+      //   });
+      // });
+
+      // d.describe('class definitions', function(d) {
+      //   d.it('parses the namespace, name, superclas, and body', function(a) {
+      //     parses(a, "class A::B < C
+      //                  1
+      //                end",
+      //        OpenClass(GetNs(
+      //          Const(GetNs(  Const(GetNs(Default, "A")), "B")),
+      //          Const(GetNs(Default, "C")),
+      //          Integer(1)
+      //        )));
+      //   });
+
+      //   d.it('parses defaults there is no namespace, superclass, or body', function(a) {
+      //     parses(a, "class A; end", OpenClass(GetNs(
+      //                                 Const(GetNs(Default, "A")),
+      //                                 Default,
+      //                                 Default
+      //                               )));
+      //   });
+
+      //   d.it('parses nested declarations', function(a) {
+      //     parses(a, "class A; class B; end; end",
+      //       OpenClass(GetNs(
+      //         Const(GetNs(Default, "A")),
+      //         Default,
+      //         OpenClass(GetNs(
+      //           Const(GetNs(Default, "B")),
+      //           Default,
+      //           Default
+      //         ))
+      //       ))
+      //     );
+      //   });
+      // });
+
+      // d.describe('module definitions', function(d) {
+      // });
+
+      // d.describe('method definitions', function(d) {
+      //   d.example('with no args or body', function(a) {
+      //     parses(a, "def bland_method; end", Def(Start("bland_method", [], Default)));
+      //   });
+      //   d.example('with a body', function(a) {
+      //     parses(a, "def hasbody; true; end", Def(Start("hasbody", [], True({begin:13, end:17}))));
+      //   });
+      //   d.example('with required and rest args', function(a) {
+      //     parses(a, "def hasargs(req, *rst); end",
+      //         Def(Start("hasargs",
+      //           [Required("req"), Rest("rst")],
+      //           Default
+      //         )));
+      //   });
+      // });
+    });
+  }
+}
