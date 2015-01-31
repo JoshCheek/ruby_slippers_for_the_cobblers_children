e497870d5decc7de4b580ca2762d7bcc8c5be487
Josh Cheek <josh.cheek@gmail.com>
2015-01-30 17:07:30 -0700

    Refactor Stack
    Was just too hard to read with the @:forward,
    found reading the source to be confusing.

--- interpreter/src/Stack.hx
+++ interpreter/src/Stack.hx
@@ -1,18 +1,22 @@
-@:forward(length, pop)
 abstract Stack<T>(List<T>) {
-  public var peek(get, never):T;
-  public var isEmpty(get, never):Bool;
 
-  public function new() {
+  public function new()
     this = new List<T>();
-  }
+
+  public var peek    (get, never):T;
+  public var isEmpty (get, never):Bool;
+  public var length  (get, never):Int;
 
   inline public function push(datum:T):T {
     this.push(datum);
     return datum;
   }
 
+  inline public function pop():T
+    return this.pop();
+
   // private
   inline function get_peek()    return this.first();
   inline function get_isEmpty() return this.isEmpty();
+  inline function get_length()  return this.length;
 }
