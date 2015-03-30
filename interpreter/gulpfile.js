01e63d6d02cc780fc09e7026699bbac0f5b8ad5f
Josh Cheek <josh.cheek@gmail.com>
2015-03-29 21:47:24 -0600

    Fuuuuuck, Seriously, its been like 3 or 4 hours now. Fuck Gulp.

--- interpreter/gulpfile.js
+++ interpreter/gulpfile.js
@@ -5,13 +5,24 @@ var mocha   = require('gulp-mocha');
 // var EmitJson = function() {
 // }
 
-var File = require('vinyl');
-var JsonFile = new File({
-  cwd: "/",
-  base: "/",
-  path: "/package.json",
-  contents: new Buffer("test = 123")
-});
+// faking a stream (and fuck you, Gulp, for making it take 2 fucking hours to figure this out)
+// https://nodejs.org/api/stream.html#stream_api_for_stream_implementors
+var stream = require('stream');
+
+var MyStreamReadable = function() {
+  stream.Readable.call(this);
+}
+
+// You *MUST* inherit first, or it will overwrite the method
+// https://nodejs.org/api/util.html#util_util_inherits_constructor_superconstructor
+require("util").inherits(MyStreamReadable, stream.Readable);
+MyStreamReadable.prototype._read = function(numBytes) {
+  // https://nodejs.org/api/stream.html#stream_readable_read_size_1
+  // this fails, because file.relative is "undefined" in vinyl.
+  // no fucking goddam clue why
+  this.push('omg hello');
+}
+
 
 gulp.task('default', function() {
   return gulp.src('src/app.js')
@@ -25,12 +36,6 @@ gulp.task('test', function() {
 });
 
 gulp.task('package', function() {
-  // gulp.dest('package.json').end(new JsonFile())
-  // return new JsonFile().pipe(gulp.dest('package.json'));
-
   var dest = gulp.dest('package.json');
-  return function(writer, data, encoding, callback) {
-    dest(writer, "OMG", encoding, callback)
-  };
-
+  return new MyStreamReadable().pipe(dest);
 });
