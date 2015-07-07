e393c8b14bb44e61c1f3549e3a98084ff98e8735
Josh Cheek <josh.cheek@gmail.com>
2015-07-06 20:05:26 -0600

    Hopefully better state management

--- interpreter/src/vm.js
+++ interpreter/src/vm.js
@@ -2,6 +2,12 @@ var VM = function(world) {
   this.world = world
 }
 
+VM.prototype.nextExpression = function() {
+  const statestack  = this.world.statestack
+  while(!this.step(statestack)) { }
+  return this.currentExpression()
+}
+
 VM.prototype.currentBinding = function() {
   return this.world.callstack[this.world.callstack.length-1]
 }
@@ -15,33 +21,92 @@ VM.prototype.lookup = function(id) {
   return this.world.allObjects[id]
 }
 
+VM.prototype.step = function(statestack) {
+  const state       = statestack[statestack.length-1]
+  const resultState = this.stepMain(state)
+  return this.handleStepResult(statestack, state, resultState)
+}
 
-VM.prototype.advanceState = function() {
-  let rTrue = this.world.rTrue
-
-  let tmpBullshit = function(ast) {
-    switch(ast.type) {
-      case "true":
-        this.currentBinding().returnValue = rTrue.objectId
-        return rTrue
-      default: throw(new Error(`Unexpected ast: ${ast.type}`))
-    }
+VM.prototype.handleStepResult = function(statestack, state, resultState) {
+  switch(resultState.type) {
+    case "push":
+      state.name = resultState.returnState
+      statestack.push({name: resultState.pushThis, registers: {}})
+      return false
+    case "advance":
+      state.name = resultState.nextState
+      return false
+    case "pop":
+      statestack.pop()
+      return resultState.expressionComplete
+    case "noop":
+      return resultState.expressionComplete
+    default: throw(new Error(`Unexpected state action: ${resultState}`))
   }
+}
 
-  const state = this.world.state
+VM.prototype.stepMain = function(state) {
+  const actions    = this.step.actions
   switch(state.name) {
     case "start":
-      this.world.state = {name: "finished"}
-      return tmpBullshit.call(this, this.world.ast)
+      state.substateStack = [this.astMachine(this.world.ast)]
+      return actions.advance("running")
+
+    case "running":
+      const substack    = state.substateStack
+      if(substack.length == 0)
+        return actions.advance("finished")
+      const state           = substack[substack.length-1]
+      const expressionFound = this.handleStepResult(substack, state, this.stepRunning(state))
+      return actions.noop(expressionFound)
+
     case "finished":
-      return this.world.rNil
-    default: throw(new Error(`Unexpected state: ${this.world.state}`))
+      this.currentBinding().returnValue = this.world.rNil.objectId
+      return actions.noop(true)
+
+    default: throw(new Error(`Unexpected state: ${JSON.stringify(state)}`))
   }
 }
 
-VM.prototype.nextExpression = function() {
-  return this.advanceState()
+VM.prototype.step.actions = {
+  push: function(pushStateName, returnStateName) {
+    return {type: "push", pushThis: pushStateName, returnState: returnStateName}
+  },
+  advance: function(nextState) {
+    return {type: "advance", nextState: nextState}
+  },
+  pop: function(expressionComplete) {
+    return {type: "pop", expressionComplete: expressionComplete}
+  },
+  noop: function(expressionComplete) {
+    return {type: "noop", expressionComplete: expressionComplete}
+  },
 }
+
+VM.prototype.astMachine = function(ast) {
+  const Type    = ast.type[0].toUpperCase() + ast.type.slice(1, ast.type.length)
+  const state   = {
+    name:      `eval${Type}`,
+    ast:       ast,
+    registers: {},
+  }
+
+  switch(ast.type) {
+    case "true": return state
+    default: throw(new Error(`Unexpected ast: ${ast.type}`))
+  }
+}
+
+VM.prototype.stepRunning = function(state) {
+  const actions = this.step.actions
+  switch(state.ast.type) {
+    case "true":
+      this.currentBinding().returnValue = this.world.rTrue.objectId
+      return actions.pop(true);
+    default: throw(new Error(`Unexpected ast: ${ast.type}`))
+  }
+}
+
 
 
 VM.bootstrap = function(ast) {
@@ -116,7 +181,7 @@ VM.bootstrap = function(ast) {
   // put it all together
   let world = {
     ast:        ast,
-    state:      {name: "start"},
+    statestack: [{name: "start", registers: {}}],
     rNil:       rNil,
     rTrue:      rTrue,
     callstack:  callstack,
