590269442c279f310d7aa6f309f2fcf2fbfffe8f
Josh Cheek <josh.cheek@gmail.com>
2015-07-22 02:56:51 -0600

    Frontend: Elm: Better parsing and display

--- frontend/experiments/elm/HttpExample.elm
+++ frontend/experiments/elm/HttpExample.elm
@@ -1,36 +1,55 @@
 import Http
 import Html exposing (Html)
 import Task exposing (Task)
-import Json.Decode as Json exposing ((:=))
-import Graphics.Element exposing (show)
+import Json.Decode as Json exposing ((:=), value)
+import Graphics.Element exposing (Element, show)
+import String exposing (toInt)
+import Text
 
 
 type Ast
   = AstUninitialized               -- This is stupid, but the mailbox needs an initial value
   | AstNone                        -- For when there is no value
-  | AstInt String                  -- value, should be an int
+  | AstInt Int                     -- value
   | AstString String               -- value
   | AstSymbol String               -- value
   | AstSend Ast String (List Ast)  -- target, message, args
   | AstUnknown String              -- show the type, ie if we forget to parse one
 
+toString : Ast -> String
+toString ast = case ast of
+  AstUninitialized            -> "Uninitialized"
+  AstNone                     -> "null"
+  AstInt n                    -> Basics.toString n
+  AstString str               -> Basics.toString str
+  AstSymbol sym               -> ":" ++ (Basics.toString sym)
+  AstSend target message args -> (toString target) ++ "." ++ message ++ "(" ++ (String.join ", " (List.map toString args)) ++ ")"
+  AstUnknown typ              -> "Unknown: " ++ typ
+
+show : Ast -> Element
+show ast =
+  Graphics.Element.leftAligned (Text.monospace (Text.fromString (toString ast)))
+
+
 parsedCode : Signal.Mailbox Ast
 parsedCode = Signal.mailbox AstUninitialized
 
--- main = show (Json.decodeString decoder "{\"type\": \"integer\", \"value\": \"1\"}")
+
+main : Signal Graphics.Element.Element
 main = Signal.map show parsedCode.signal
+
 
 port fetchCode : Task Http.Error ()
 port fetchCode =
   Http.post decodeAst "http://localhost:3003" (Http.string "1.a 'b', :c, d")
     `Task.andThen` report
 
+
 report : Ast -> Task x ()
 report result =
   Signal.send parsedCode.address result
 
 
--- NOTE: toplevel can be null
 decodeAst : Json.Decoder Ast
 decodeAst =
   Json.oneOf
@@ -45,30 +64,19 @@ decodeAst =
           otherwise -> decodeUnknown
   , Json.null AstNone
   -- this shouldn't ever happen, but it already has (https://github.com/elm-lang/core/issues/305), so, leaving it in
-  , Json.customDecoder Json.value (\json -> (Ok <| AstUnknown (toString json)))
+  , Json.customDecoder Json.value (\json -> (Ok <| AstUnknown (Basics.toString json)))
   ]
 
-decodeInt     = Json.object1 AstInt     ("value"   := Json.string)
-decodeUnknown = Json.object1 AstUnknown ("type"    := Json.string)
-decodeSend    = Json.object3 AstSend    ("target"  := decodeAst)
-                                        ("message" := Json.string)
-                                        ("args"    := Json.list decodeAst)
-decodeString  = Json.object1 AstString  ("value" := Json.string)
-decodeSymbol  = Json.object1 AstSymbol  ("value" := Json.string)
+decodeUnknown = Json.object1 AstUnknown ("type"   := Json.string)
+decodeString  = Json.object1 AstString  ("value"  := Json.string)
+decodeSymbol  = Json.object1 AstSymbol  ("value"  := Json.string)
 
--- An example of what we have to parse (minus locations)
--- a 1 + 2, "b", :d
---
--- { "type": "send",
---   "target": null,
---   "message": "a",
---   "args": [
---     { "type": "send",
---       "target": { "type": "integer", "value": "1" },
---       "message": "+",
---       "args": [{ "type": "integer", "value": "2" }],
---     },
---     { "type": "string", "value": "b" },
---     { "type": "symbol", "value": "d" }
---   ],
--- }
+decodeInt =
+  let toAstInt s = case (toInt s) of Ok i -> AstInt i
+                                     _    -> AstInt 0 -- shouldn't ever happen
+  in Json.object1 toAstInt ("value" := Json.string)
+
+decodeSend = Json.object3 AstSend
+                          ("target" := decodeAst)
+                          ("message" := Json.string)
+                          ("args" := Json.list decodeAst)
