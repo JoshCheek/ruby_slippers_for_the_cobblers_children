1521f4eb58a609c03f4adfae3dc4fa0137ac061b
Josh Cheek <josh.cheek@gmail.com>
2015-07-24 05:26:38 -0600

    Frontend: Camera and renderer update when dimensions of dom change

--- frontend/js/DisplayRuby.js
+++ frontend/js/DisplayRuby.js
@@ -1,25 +1,13 @@
 var DisplayRuby = function(attributes) {
-  // ambient light
+  // lights
   var ambientLight = new THREE.AmbientLight(0x0c0c0c)
-
-  // spotlight (only works on MeshLambertMaterial and MeshPhongMaterial)
-  var spotLight  = new THREE.SpotLight(0xffffff)
+  var spotLight    = new THREE.SpotLight(0xffffff)
   spotLight.position.set(0, 5, -10)
-  spotLight.castShadow       = true
-
-  spotLight.shadowMapWidth   = 1024
-  spotLight.shadowMapHeight  = 1024
-
-  spotLight.shadowCameraNear = 500
-  spotLight.shadowCameraFar  = 4000
-  spotLight.shadowCameraFov  = 30
-
-  spotLight.intensity        = 0.9
+  spotLight.intensity = 0.9
 
   // temporary cube to prove this setup works
-  var geometry = new THREE.BoxGeometry(1, 1, 1)
-  var material = new THREE.MeshLambertMaterial({color: 0xffff00})
-  this.cube    = new THREE.Mesh(geometry, material)
+  this.cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1),
+                             new THREE.MeshLambertMaterial({color: 0xffff00}))
 
   // scene
   this.scene = new THREE.Scene()
@@ -30,18 +18,16 @@ var DisplayRuby = function(attributes) {
   // renderer
   this.renderer = new THREE.WebGLRenderer()
   this.renderer.setClearColor(0x222222, 1.0)
-  this.renderer.shadowMapEnabled = false // calculates shadows when true (this is apparently expensive, so turn it off if we don't wind up needing it)
+  this.renderer.shadowMapEnabled = false
 
   // camera
-  this.camera = new THREE.PerspectiveCamera(45,   // field of view
-                                            16/9, // aspect ratio (will be updated when we set the size)
-                                            0.1,  // near plane (closest thing you can see)
-                                            100)  // far plane (farthest thing you can see)
+  this.camera = new THREE.PerspectiveCamera(45, 16/9, 0.1, 100)
   this.camera.position.x = 0
   this.camera.position.y = 0
   this.camera.position.z = -10
   this.camera.lookAt(new THREE.Vector3(0, 0, 0)) // origin
 
+  // set width and height
   this.updateSize(attributes.width, attributes.height)
 
   // iteration
@@ -59,7 +45,8 @@ DisplayRuby.prototype.updateSize   = function(width, height) {
   this.height      = height
   this.aspectRatio = width / height
   this.renderer.setSize(width, height)
-  // this.camera.aspect(this.aspectRatio)
+  this.camera.aspect = this.aspectRatio
+  this.camera.updateProjectionMatrix()
   return this
 }
 
